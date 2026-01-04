c561da58316d7c7f8eac8909d61af47850c3936b
tharshihan2000@gmail.com <tharshihan_r_e23@engug.ruh.ac.lk>
2026-01-04 21:22:13 +0530

    Auth is done

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
-//import Navbar from "@/components/Navbar";
+import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
 
 const geistSans = Geist({
@@ -31,7 +31,7 @@ export default function RootLayout({
         <body
           className={`${geistSans.variable} ${geistMono.variable} antialiased`}
         >
-          {/* <Navbar /> */}
+          <Navbar />
           {children}
           <Footer />
         </body>
